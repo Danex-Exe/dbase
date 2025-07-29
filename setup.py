6660aad6e0fbb3acea18524d94410d993e62dec3
Daniil Alekseev <125923529+Danex-Exe@users.noreply.github.com>
2025-07-29 11:59:08 +0300

    Update setup.py
    fix bugs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description='The Python module provides classes for simplified file management and synchronization. The `DataFile` class allows you to create, read, write, rename, and delete files, as well as get their information. It supports working with files in JSON format and with the ability to keep logs',
     author='Daniil Alekseev',
     author_email='dan.d.alekseev@gmail.com',
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     install_requires=["cryptography"],
     python_requires=">=3.8",
