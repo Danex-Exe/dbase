7b53668ff91bcef5631e17a0a40500ec5602efb8
Daniil Alekseev <125923529+Danex-Exe@users.noreply.github.com>
2025-08-18 15:40:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description='DBase - Secure Python DB library. Manage text/JSON/encrypted databases with built-in logging and error handling. Military-grade encryption, temp DBs, atomic ops. Ideal for secure local storage',
     author='Daniil Alekseev',
     author_email='sevenaspects.mail@gmail.com',
-    version="2.0.5",
+    version="2.0.6",
     packages=find_packages(),
     install_requires=["cryptography"],
     python_requires=">=3.8",
